83b8ffcd830c73428b66aec89314d25fe379174b
Vaishnavi-Kausale <vaishnavikausale@gmail.com>
2025-01-30 12:09:59 +0530

    removed unuse imports

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -3,7 +3,7 @@ import 'package:quiz_app/data/questions.dart';
 import 'package:quiz_app/result_screen.dart';
 import 'package:quiz_app/start_screen.dart';
 import 'package:quiz_app/questions_screen.dart';
-import 'package:quiz_app/models/quiz_question.dart';
+
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/data/questions.dart';
 import 'package:quiz_app/questions_summary.dart';
-import 'package:quiz_app/models/quiz_question.dart';
 
 class ResultScreen extends StatelessWidget {
   const ResultScreen({super.key, required this.chosenAnswers});
